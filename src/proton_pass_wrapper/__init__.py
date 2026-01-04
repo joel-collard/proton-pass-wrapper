203cc227613d204bfc253d4465fb1723fd4a293c
Anonymous <140573961+joel-collard@users.noreply.github.com>
2026-01-04 09:47:47 -0500

    Add __init__.py

---
+++ src/proton_pass_wrapper/__init__.py
@@ -0,0 +1,19 @@
+from .core import (
+    protonpass_path,
+    protonpass_status,
+    protonpass_login,
+    protonpass_logout,
+    protonpass_get,
+    protonpass_items,
+)
+
+__all__ = [
+    "protonpass_path",
+    "protonpass_status",
+    "protonpass_login",
+    "protonpass_logout",
+    "protonpass_get",
+    "protonpass_items",
+]
+
+__version__ = "1.0.0"
