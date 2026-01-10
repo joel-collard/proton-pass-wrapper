87b2b5d648c284ff5aee794ec833f61888115f70
Anonymous <140573961+joel-collard@users.noreply.github.com>
2026-01-09 21:52:12 -0500

    Updated __init__.py

--- src/proton_pass_wrapper/__init__.py
+++ src/proton_pass_wrapper/__init__.py
@@ -3,7 +3,7 @@
 It enables easy, private, and secure access to your vaults, in local or headless environments, allowing you to retrieve and inject secrets 
 programmatically while keeping them out of your source code. Designed for versatility, it works out-of-the-box on Windows, macOS, and Linux.
 
-📄 Techincal reference: https://github.com/joel-collard/proton-pass-wrapper
+📄 Technical reference: https://github.com/joel-collard/proton-pass-wrapper
 """
 
 # 1. Import functions from core.py to make them accessible at the top level
